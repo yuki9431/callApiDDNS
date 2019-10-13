a088c7f3d3a90f0dc9f9557d30b2b45d6d8dbd7d
yuki9431 <yuki9431@gmail.com>
2019-10-13 15:37:05 +0900

    first commit

---
+++ callApiDDNS.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# IPアドレスを更新する
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+LOGFILE=/var/log/callApiDdns.log
+DOMAIN=
+APITOKEN=
+
+echo -n "/usr/local/shell/callApiDDNS.sh start at `date '+%Y/%m/%d %R'` " >> $LOGFILE
+echo -n "Result: " >> $LOGFILE
+curl -X GET https://f5.si/update.php?domain=${DOMAIN}\&password=${APITOKEN} >> $LOGFILE
+echo "" >> $LOGFILE
+
+exit 0
